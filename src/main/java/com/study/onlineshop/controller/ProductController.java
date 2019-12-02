8a751919d90434668272b0d8b8a937c32993ae7a
Admin <esuturin77@gmail.com>
2019-12-02 23:20:39 +0200

    final version

--- src/main/java/com/study/onlineshop/controller/ProductController.java
+++ src/main/java/com/study/onlineshop/controller/ProductController.java
@@ -1,11 +1,11 @@
 package com.study.onlineshop.controller;
 
 import com.study.onlineshop.dao.ProductDao;
+import com.study.onlineshop.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
@@ -14,6 +14,39 @@ public class ProductController {
 
     @RequestMapping(value = "products", method= RequestMethod.GET)
     public String getAllProducts(Model model){
+        model.addAttribute("products", productDao.getAll());
+        return "products";
+    }
+
+    @GetMapping(path = "/product/add")
+    public String addProduct(){
+        return "add";
+    }
+
+    @RequestMapping(value = "/product/add", method = RequestMethod.POST)
+    public String addProductPost(Product product, Model model){
+        productDao.add(product);
+        model.addAttribute("products", productDao.getAll());
+        return "products";
+    }
+
+    @GetMapping(path = "/product/edit/{id}")
+    public String editProduct(@PathVariable int id, Model model){
+        model.addAttribute("product", productDao.getById(id));
+        return "edit";
+    }
+
+
+    @RequestMapping(value = "/product/edit/{id}", method = RequestMethod.POST)
+    public String editProduct(Product product, Model model){
+        productDao.update(product);
+        model.addAttribute("products", productDao.getAll());
+        return "products";
+    }
+
+    @RequestMapping(value = "/product/delete/{id}", method = RequestMethod.POST)
+    public String deleteProduct(@PathVariable int id, Model model){
+        productDao.delete(id);
         model.addAttribute("products", productDao.getAll());
         return "products";
     }
